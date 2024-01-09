eff29eccc1a30075ba4b0d8427e1d2bf58a17b85
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-01-09 12:38:19 -0300

    Desafio
    Desafio

---
+++ Muito fácil/Encontre o maior numero do vetor.cs
@@ -0,0 +1,30 @@
+﻿int[] vet = new int[]{};
+
+maiorNumeroVetor(vet);
+
+int maiorNumeroVetor(int[] vet)
+{
+	if (vet.Length > 0)
+	{
+		int n = vet[0];
+
+		for (int i = 1; i < vet.Length; i++)
+		{
+			if (vet[i] > n)
+			{
+				n = vet[i];
+			}
+		}
+
+		Console.WriteLine(n + " Maior numero do vetor");
+		return n;
+	}
+	else
+	{
+		Console.WriteLine("Vetor vazio");
+		return 0;
+	}
+}
+
+
+
